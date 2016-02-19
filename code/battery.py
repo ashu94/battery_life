daa0e5bec9809ad48ede5d986ffaf48a771aca21
ashu94 <singhjug1994@gmail.com>
2016-02-19 20:29:52 +0530

    code added

---
+++ code/battery.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python2
+import fnmatch,os
+import re,time
+from pyfirmata import *
+from serial.tools.list_ports import comports
+
+# This section load the `boards.txt` file in a dictionary of the following format
+# { 'model0': ['Name of arduino board',
+#             vid,
+#             pid],
+# 'model1': ['Name of arduino board',
+#             vid,
+#             pid]
+# ... }
+# where vid and pid are two lists of the same length, and vid[i]:pid[i] are the
+# vendor id and product id couple for the board
+
+boards_file = open('./boards.txt', 'r')
+boards_lines = boards_file.readlines()
+
+model = {}
+
+for line in boards_lines:
+	match = re.search(r'(^[a-z]*)\.name=(.*)', line)
+	if match:
+		model[match.group(1)] = [match.group(2), [], []] # the two empty lists are the vid and pid lists
+
+for line in boards_lines:
+	for k in model.keys():
+		match_vid = re.search(r'^' + k + r'\.vid\.[0-9]*=0x([a-fA-F0-9]{4})', line)
+		match_pid = re.search(r'^' + k + r'\.pid\.[0-9]*=0x([a-fA-F0-9]{4})', line)
+		if match_vid:
+			model[k][1].append(match_vid.group(1).upper())
+		elif match_pid:
+			model[k][2].append(match_pid.group(1).upper())
+
+prods = {'0001':'Arduino Uno'      , '0043':'Arduino Uno R3',
+		 '0010':'Arduino Mega 2560', '0042':'Arduino Mega 2560 R3',
+		 '003F':'Arduino Mega ADK' , '0044':'Arduino Mega ADK R3'}
+
+#Arduino vendor ID: 0x2341
+#Old Arduinos use the FTDI VID/PID (VID=0x0403)
+VIDs  = ['2341', '0403']
+
+sdevs = comports()
+intdev = []
+for dev in sdevs:
+	if dev[2]!= 'n/a':
+		intdev.append(dev)
+
+ptn = 'USB VID:PID=([A-F0-9]{4}):([A-F0-9]{4}) '
+arduinos = []
+for dev in intdev:
+	match = re.search(ptn,dev[2])
+	if match and match.group(1) == VIDs[0]:
+		if match.group(2) in prods:
+			data = (prods[match.group(2)], dev[0])
+		else:
+			data = ('Arduino', dev[0])
+	elif match and match.group(1) == VIDs[1]:
+			data = ('FTDI', dev[0])
+	else:
+		data = ('Unknown', dev[0])
+	arduinos.append(data)
+
+if not arduinos:
+	print 'No arduino connected !!!'
+else:
+	print 'Connected Arduino device(s):'
+	for ard in arduinos:
+		print ard[0], ' at ', ard[1] 
+found = 0
+board = Arduino(ard[1])
+for file in os.listdir('/sys/class/power_supply/'):
+     if fnmatch.fnmatch(file, 'BAT*'):
+      print file+" contains all the battery information"
+      found += 1
+if found == 0 :
+  print "no battery information file found"
+#      print file
+#board.digital[8].write(1)
+#cat /sys/class/power_supply/BAT1/capacity
+#f = open('/sys/class/power_supply/BAT1/capacity', 'r')
+#print f.read()
+while 1:
+  with open('/sys/class/power_supply/'+file+'/capacity', 'r') as f:
+    percentage = int(f.readlines()[0])
+  print percentage
+  if percentage > 80:
+    board.digital[8].write(0)
+  if percentage < 60:
+    board.digital[8].write(1)
+  elif percentage<80 and percentage>60:
+    board.digital[8].write(1)
+  time.sleep(5)
+    
+
